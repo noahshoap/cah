f23fc725a0fbbd220189f020ba4747357833286d
Noah Shoap <noahshoap@gmail.com>
2025-03-23 17:37:51 -0400

    forgot to make actually abstract

--- cah/Abstractions/AbstractGameMode.cs
+++ cah/Abstractions/AbstractGameMode.cs
@@ -1,6 +1,6 @@
 namespace cah.Abstractions;
 
-public class AbstractGameMode(Guid id) : IGame
+public abstract class AbstractGameMode(Guid id) : IGame
 {
     protected Dictionary<string, string> Players = new();
     public Guid Id { get; } = id;
